1217dbae8b93695f99e24fe7f0adb33a3bcec5f3
Sarah Flynn <sarah.flynn@icloud.com>
2018-10-22 07:37:18 -0700

    very basic validation of User model passing

---
+++ test/unit/User.test.js
@@ -0,0 +1,35 @@
+const { getErrors } = require('./helpers/getErrors');
+const User = require('../../lib/models/User');
+
+describe('User Model', () => {
+    it('accepts a valid user when passed', () => {
+        const data = {
+            fullName: 'Douglas Fir',
+            preferredName: 'Doug',
+            email: 'dfir@gmail.com',
+            role: 'dog-seeker',
+            preferredContact: {
+                text: 5035554444,
+                comments: 'Nights and weekends are best'
+            },
+            address: {
+                city: 'Portland',
+                state: 'OR',
+                zip: 97205
+            }
+        };
+
+        const user = new User(data);
+        const jsonUser = user.toJSON();
+        expect(jsonUser).toEqual({ ...data, _id: expect.any(Object) });
+    });
+
+    it('validates that required fields are included', () => {
+        const user = new User({
+            preferredName: 'Doug'
+        });
+
+        const errors = getErrors(user.validateSync(), 4);
+        expect(errors.fullName.properties.message).toEqual('Name is required.');
+    });
+});
