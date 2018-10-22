0ff14cd0093ee60f89dd09633d84919eb5995cc5
David Chhing <34200452+DavidChhing@users.noreply.github.com>
2018-10-22 12:12:39 -0700

    David breed model (#2)
    * working on breed model
    
    * finished writing breed model and updated package json file
    
    * updated breed model
    
    * writing breed unit testZ

---
+++ test/unit/breed.test.js
@@ -0,0 +1,40 @@
+const { getErrors } = require('./helpers/getErrors');
+const Breed = require('../../lib/models/Breed');
+
+describe('Breed model', () => {
+
+    it('Validates a good model', () => {
+        
+        const data = {
+            breed: 'Samoyed',
+            weightRange: '41 to 50 lbs',
+            lifespan: 15,
+            temperament: [],
+            coatTypes: 'Smooth',
+            hypoallergenic: true,
+            shed: true
+        };
+
+        const breed = new Breed(data);
+        
+
+
+
+    });
+
+
+
+
+
+
+
+    // it('validates that required fields are included', () => {
+    //     const breed = new Breed({
+    //         preferredName: 'Doug'
+    //     });
+
+    //     const errors = getErrors(breed.validateSync(), 4);
+    //     expect(errors.breed.properties.message).toEqual('breed is required.');
+    // });
+
+});
