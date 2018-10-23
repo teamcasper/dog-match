a3fe83695395739b8485793d2b4d66202c62bd10
KLPainter <KLPainter@gmail.com>
2018-10-23 10:58:57 -0700

    zip-api test passing

--- test/unit/zip-api.test.js
+++ test/unit/zip-api.test.js
@@ -1,12 +1,24 @@
 const zipApi = require('../../lib/util/zip-api');
 
-describe('blah', () => {
+describe('test zip-api', () => {
 
-    it('', () => {
+    it('returns an array of zip codes', () => {
         return zipApi('97229', 5)
             .then(zipCodes => {
-                expect(zipCodes).toEqual([]);
+                expect(zipCodes).toEqual([
+                    '97078',
+                    '97075',
+                    '97076',
+                    '97077',
+                    '97005',
+                    '97298',
+                    '97003',
+                    '97225',
+                    '97006',
+                    '97296',
+                    '97291',
+                    '97210',
+                    '97229']);
             });
     });
-
 });
