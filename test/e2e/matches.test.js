a6c3fc608fa5a563354e23131c25995d61957d74
KLPainter <KLPainter@gmail.com>
2018-10-22 17:58:24 -0700

    all but one test passing

--- test/e2e/matches.test.js
+++ test/e2e/matches.test.js
@@ -36,4 +36,31 @@ describe('matches routes', () => {
                 expect(res.body).toContainEqual(createdMatches[2]);
             });
     });
+
+    it('deletes a match by id', () => {
+        const createdMatches = getMatches();
+
+        return request(app)
+            .delete(`/api/matches/${createdMatches[1]._id}`)
+            .then(() => request(app).get('/api/matches'))
+            .then(res => {
+                expect(res.body).not.toContainEqual(createdMatches[1]);
+                expect(res.body).toContainEqual(createdMatches[0]);
+                expect(res.body).toContainEqual(createdMatches[2]);
+            });
+    });
+
+    // it('updates a match by id', () => {
+    //     const createdMatches = getMatches();
+    //     let data = createdMatches[1];
+    //     data.feePaid = 1000;
+
+    //     return request(app)
+    //         .put(`/api/matches/${createdMatches[1]._id}`)
+    //         .send({ data })
+    //         .then(res => {
+    //             expect(res.body).toEqual({ data });            
+    //         }); 
+    // });
+
 });
